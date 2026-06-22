b444fedd5ae3f213c2803794ef41b9dde48ccdea
Osman-arif <uarif5162@gmail.com>
2026-06-22 22:44:42 +0500

    changes in app.js to verify automatic trigger

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 http.createServer((req,res)=>{
 	res.writeHead(200);
-	res.end("tadaun Hello from jenkin pipeline ! usman se242005");
+	res.end("automatic build now trigger ! usman se242005");
 }).listen(3000, () => console.log("app running on port 3000"));
