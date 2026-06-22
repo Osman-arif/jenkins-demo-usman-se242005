b11a83b2c57a711240977f27d48b8552f15cdb91
Osman-arif <154276077+Osman-arif@users.noreply.github.com>
2026-06-22 14:55:50 +0300

    Fix syntax error in server listen callback

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const http = require('http');
 http.createServer((req,res)=>{
 	res.writeHead(200);
 	res.end("Hello from jenkin pipeline ! usman se242005")
-}).listen(3000,=> console.log("app running on port 3000"));
+}).listen(3000, () => console.log("app running on port 3000"));
